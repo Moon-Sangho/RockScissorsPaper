9bc4577b7fd8ef8575bf16725c5d4ee0340a169a
Moon-Sangho <skym887@gmail.com>
2020-07-23 20:26:54 +0900

    3rd commit

--- main.js
+++ main.js
@@ -1,29 +1,34 @@
-let computerPick = '0';
-let dictionary = { // 딕셔너리 자료구조
-    rock: '0',
-    scissor: '-135px',
-    paper: '-284px'
+let imageCoordinate = '0';
+const dictionary = { // 딕셔너리 자료구조
+    바위: '0',
+    가위: '-135px',
+    보: '-284px'
 };
 
+console.log(Object.entries(dictionary));
+function computerPick(imageCoordinate) {
+    return Object.entries(dictionary).find(function(v) {
+        return v[1] === imageCoordinate;
+    })[0];
+}
+
+
 setInterval(function() {
-    if (computerPick === dictionary.rock) {
-        computerPick = dictionary.scissor
-    } else if (computerPick === dictionary.scissor) {
-        computerPick = dictionary.paper;
+    if (imageCoordinate === dictionary.바위) {
+        imageCoordinate = dictionary.가위
+    } else if (imageCoordinate === dictionary.가위) {
+        imageCoordinate = dictionary.보;
     } else {
-        computerPick = dictionary.rock;
+        imageCoordinate = dictionary.바위;
     }
     document.querySelector("#computer").style.background =
-        'url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ' + computerPick + ' 0';
+        'url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ' + imageCoordinate + ' 0';
 }, 100); 
 
 
 document.querySelectorAll('.btn').forEach(function(btn) {
     btn.addEventListener('click', function() {
         let myPick = this.textContent;
-        console.log(myPick, computerPick);
+        console.log(myPick, computerPick(imageCoordinate));
     });
 });
-
-
-

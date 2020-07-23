b6889fa61f0f7249f85cd3b8c2017394e19e96f0
Moon-Sangho <skym887@gmail.com>
2020-07-23 22:18:52 +0900

    4th commit

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const dictionary = { // 딕셔너리 자료구조
     보: '-284px'
 };
 
-console.log(Object.entries(dictionary));
 function computerPick(imageCoordinate) {
     return Object.entries(dictionary).find(function(v) {
         return v[1] === imageCoordinate;
@@ -13,7 +12,7 @@ function computerPick(imageCoordinate) {
 }
 
 
-setInterval(function() {
+let interval = setInterval(function() {
     if (imageCoordinate === dictionary.바위) {
         imageCoordinate = dictionary.가위
     } else if (imageCoordinate === dictionary.가위) {
@@ -25,10 +24,47 @@ setInterval(function() {
         'url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ' + imageCoordinate + ' 0';
 }, 100); 
 
-
 document.querySelectorAll('.btn').forEach(function(btn) {
     btn.addEventListener('click', function() {
+        clearInterval(interval);
+        setTimeout(function () {
+            interval = setInterval(function() {
+                if (imageCoordinate === dictionary.바위) {
+                    imageCoordinate = dictionary.가위
+                } else if (imageCoordinate === dictionary.가위) {
+                    imageCoordinate = dictionary.보;
+                } else {
+                    imageCoordinate = dictionary.바위;
+                }
+                document.querySelector("#computer").style.background =
+                    'url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ' + imageCoordinate + ' 0';
+            }, 100); 
+        }, 1000);
         let myPick = this.textContent;
-        console.log(myPick, computerPick(imageCoordinate));
+        if (myPick === '가위') {
+            if (computerPick(imageCoordinate) === '가위') {
+                console.log('Draw!');
+            } else if (computerPick(imageCoordinate) === ' 바위') {
+                console.log('You lose!');
+            } else {
+                console.log('You win!');
+            }
+        } else if (myPick === '바위') {
+            if (computerPick(imageCoordinate) === '가위') {
+                console.log('You win!');
+            } else if (computerPick(imageCoordinate) === ' 바위') {
+                console.log('Draw!');
+            } else {
+                console.log('You lose!');
+            }
+        } else if (myPick === '보') {
+            if (computerPick(imageCoordinate) === '가위') {
+                console.log('You lose!');
+            } else if (computerPick(imageCoordinate) === ' 바위') {
+                console.log('You win!');
+            } else {
+                console.log('Draw!');
+            }
+        }
     });
 });
